883456217325978cb28fad7d6d2318527f7ac74e
baKon <maciej@geoskygps.com>
2021-07-25 13:40:55 -0500

    Update client.ts

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -55,7 +55,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext({
     headers: {
       "content-type": "application/json",
-      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_GRAPHQL_PASS,
+      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_GRAPHQL_PASS
     },
   });
 
