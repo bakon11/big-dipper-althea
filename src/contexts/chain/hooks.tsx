4f3ec43533092ec5725fc11889d6176d3754f259
baKon <maciej@geoskygps.com>
2021-07-26 09:56:16 -0500

    Update hooks.tsx

--- src/contexts/chain/hooks.tsx
+++ src/contexts/chain/hooks.tsx
@@ -92,7 +92,7 @@ export const useValidatorsAddress = (initialstate:ChainState) => {
       
       x.validatorInfo == null ? validatorAddress = "null" : validatorAddress = x.validatorInfo.operatorAddress;
       x.validatorInfo == null ? validatorAddress = "null" : selfAddress = x.validatorInfo.selfDelegateAddress;
-      x.validatorInfo == null ? { consensusAddress } = { null } : { consensusAddress } = x.validatorInfo;
+      x.validatorInfo == null ? consensusAddress = "null" : { consensusAddress } = x.validatorInfo;
       console.log(x.validatorInfo);
       console.log(consensusAddress);
       
